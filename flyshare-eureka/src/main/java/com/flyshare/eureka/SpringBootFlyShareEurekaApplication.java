de50b6b077408555d94e67c7d287e06a90f542a6
wuxiaohai <wuxiaohai@snsoft.com.cn>
2018-08-02 17:55:21 +0800

    2018年8月2日17:55:19

--- flyshare-eureka/src/main/java/com/flyshare/eureka/SpringBootFlyShareEurekaApplication.java
+++ flyshare-eureka/src/main/java/com/flyshare/eureka/SpringBootFlyShareEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.dbjinjin.flyshare.eureka;
+package com.flyshare.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
