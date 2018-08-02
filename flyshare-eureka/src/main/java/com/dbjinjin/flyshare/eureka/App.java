4cd5c2f675d086e666f9e4ae1853d9b4e1ddda46
wuxiaohai <wuxiaohai@snsoft.com.cn>
2018-08-02 15:55:43 +0800

    2018年8月2日15:55:41

---
+++ flyshare-eureka/src/main/java/com/dbjinjin/flyshare/eureka/App.java
@@ -0,0 +1,13 @@
+package com.dbjinjin.flyshare.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
