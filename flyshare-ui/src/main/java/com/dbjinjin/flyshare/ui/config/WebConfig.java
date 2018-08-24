8775cbc0f7eae76bd9fcb7ec82c86d80bd4b3d4e
wuxiaohai <wuxiaohai@snsoft.com.cn>
2018-08-24 17:31:46 +0800

    no message

---
+++ flyshare-ui/src/main/java/com/dbjinjin/flyshare/ui/config/WebConfig.java
@@ -0,0 +1,38 @@
+package com.dbjinjin.flyshare.ui.config;
+
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+
+/**
+ * <p>标题： WebConfig</p>
+ * <p>功能： </p>
+ * <p>所属模块：</p>
+ * <p>版权： Copyright © 2018</p>
+ * <p>创建日期：2018年8月24日 下午5:22:08</p>
+ * <p>类全名：com.dbjinjin.flyshare.ui.config.WebConfig</p>
+ * 作者：wuxiaohai
+ * 初审：
+ * 复审：
+ * @version 1.0
+ */
+@Configuration
+public class WebConfig
+{
+
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters()
+	{
+		FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+		HttpMessageConverter<?> converter = fastJsonHttpMessageConverter;
+		return new HttpMessageConverters(converter);
+	}
+}
